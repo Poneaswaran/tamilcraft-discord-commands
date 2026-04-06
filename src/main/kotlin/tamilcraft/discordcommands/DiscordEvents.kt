fd7b39e3be7d90be102da8d2d1a441cf4526ce6c
Nambert Jones L <98079664+NambertJones@users.noreply.github.com>
2026-04-06 23:41:07 +0530

    Update DiscordEvents.kt

--- src/main/kotlin/tamilcraft/discordcommands/DiscordEvents.kt
+++ src/main/kotlin/tamilcraft/discordcommands/DiscordEvents.kt
@@ -7,8 +7,12 @@ class DiscordEvents {
     
     @CraterEventListener
     fun onCommandRegister(event: SlashCommandRegistrationEvent) {
+        val logger = org.slf4j.LoggerFactory.getLogger("tamilcraft-discord-commands")
+        logger.info("SDLink fired SlashCommandRegistrationEvent! Registering ${DiscordCommandsMod.db.commands.size} custom commands...")
+
         // Register your dynamic commands here from config
         DiscordCommandsMod.db.commands.forEach { configCommand ->
+            logger.info("Registering Discord Command: /${configCommand.name}")
             event.addCommand(DynamicSlashCommand(configCommand))
         }
     }
